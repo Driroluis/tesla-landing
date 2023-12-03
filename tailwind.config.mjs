bea70e40db737300d7a9a961ca59d9bf18519e3a
Driroluis <larb.237@gmail.com>
2023-12-02 19:43:02 -0500

    feet: Update Header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,9 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Gotham SSm A, sans-serif']
+		},
 		extend: {},
 	},
 	plugins: [],
